bbcb3934f932a6093cf8f98c68f114d2c2cd8d71
Franco Rodríguez <Francorod47@gmail.com>
2024-06-03 15:24:36 -0300

    new fixes

--- device_driver/build.sh
+++ device_driver/build.sh
@@ -3,6 +3,9 @@
 make clean
 make
 sudo rmmod CDD.ko 2>/dev/null
+echo "CDD_GPIO_BUTTON driver is unloaded"
+sudo sudo dmesg -C
+echo "message is cleared"
 sudo insmod CDD.ko
 echo "CDD_GPIO_BUTTON driver is loaded"
 MAJOR=$(awk '$2=="CDD_GPIO_BUTTON" {print $1}' /proc/devices)
