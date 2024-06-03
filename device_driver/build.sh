027fe74bb4a86e805fd5a718ea84a382c6d2020c
Franco Rodríguez <Francorod47@gmail.com>
2024-06-03 15:20:24 -0300

    new fixes

--- device_driver/build.sh
+++ device_driver/build.sh
@@ -5,7 +5,11 @@ make
 sudo rmmod CDD_GPIO_BUTTON 2>/dev/null
 sudo insmod CDD.ko
 echo "CDD_GPIO_BUTTON driver is loaded"
-MAJOR=$(awk '$2=="CDD_GPIO_BUTTON" {print $1}' /proc/devices)
+MAJOR=$(awk '$2=="CDD.ko" {print $1}' /proc/devices)
+if [ -z $MAJOR ]; then
+    echo "Device number is not found"
+    exit
+fi
 echo $MAJOR
 sudo mknod /dev/CDD_GPIO_BUTTON c $MAJOR 0
 sudo chmod 666 /dev/CDD_GPIO_BUTTON
